114c7471467c19a9c1ac2f9625e06eb41340cf49
ju4nc5r <31509271+ju4nc5r@users.noreply.github.com>
2023-04-19 15:00:30 -0300

    agrego pagina de js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+console.log(Hola)
+//para definir variables en js se utiliza 'let' en vez de 'var' ya que ocupan distinto espacio en memoria. 
+let nombre = "Juan Cruz"
+let apellido = "Astrada"
+
+console.log(nombre)
